6094b6ce49b16c4826d218bba39bb3858e09d0b7
Rahul Ghosh <rahulghosh096@gmail.com>
2021-01-22 17:55:40 +0600

    solved Budget Calculator problem

--- solvingProblem.js
+++ solvingProblem.js
@@ -1,4 +1,4 @@
-
+//* github link :   https://github.com/RahulGhosh305/solvingProblem-A3
 
 
 
@@ -15,6 +15,35 @@ function kilometerToMeter(value){
         return value * 1000;
     }
 }
-var result = kilometerToMeter(2.2);
-console.log(result);
+// var result = kilometerToMeter(2.2);
+// console.log(result);
 
+
+
+
+
+
+
+
+
+//* Budget Calculator Solving
+
+function budgetCalculator(watch, phone, laptop){
+    var watchCost = 50;
+    var phoneCost = 100;
+    var laptopCost = 500;
+
+    if(watch < 0 || phone < 0 || laptop < 0){
+        return 'Please Enter Positive values';
+    }
+    else if (typeof watch !== 'number' || typeof phone !== 'number' || typeof laptop !== 'number'){
+        return 'Not a number';
+    }
+    else{
+        var Amount = watch * watchCost + phone * phoneCost + laptop * laptopCost;
+        return Amount;
+    }
+}
+
+// var Sum = budgetCalculator(5, 5, 5);
+// console.log(Sum) 
